b0b975de4232eeab52449cca2e7e80b2b4db0cb6
amir <09369368949a@gmail.com>
2021-05-15 13:11:35 +0430

    minor klline get csv update

--- data/control.py
+++ data/control.py
@@ -5,26 +5,35 @@ from binance.client import Client
 import confuse
 from datetime import datetime as dat
 import datetime as dt
-
+import os
+import pathlib as path
+import json
 ##TODO make config file
 
-# %%
-import os
+#%%
+f = open ('config.json', "r")
+conf = json.load(f)["config"]
+mainPath=conf["app"]["path"]
 # %% 
 ##TODO make conncetion file
-api_key="api key"
-key="SECRET"
-client = Client(api_key, key,{"timeout": 200})
+api_key=conf["credit"]["API_KEY"]
+key=conf["credit"]["API_SECRET"]
+client = Client(api_key,key,{"timeout": 2000})
 status = client.get_system_status()
 print(status)
+# %%
+conf["app"]["path"]
 # %% get data o f sepecifc symbole at any inteval any date scope
-def get_Kline_csv(name,interval="1h",startin=dat(2017,1,1,00,1,1),endin=dat.now()):
+def get_Kline_csv(name,interval="1h",startin=dat(2020,1,1,00,1,1),endin=dat.now()):
         start=startin.strftime("%d %m,%Y")
         startff=startin.strftime("%d%m%Y")
         endff=endin.strftime("%d%m%Y")
         end=endin.strftime("%d %m,%Y")
+        dirc=mainPath+"/data/csvs/"+name+"/"+interval
+        filename="data"
+        fileformat=".csv"
         ##info = client.get_symbol_info(name) ## check for is trading or not !
-        print("from"+start+"to"+end)
+        print("from  "+start+" ->     "+end)
     ##get_historical_klines(symbol, interval, start_str, end_str=None, limit=500)
         flag=1
         while flag==1:
@@ -46,20 +55,27 @@ def get_Kline_csv(name,interval="1h",startin=dat(2017,1,1,00,1,1),endin=dat.now(
                     themplis.append(data[x][xx])
             dfdata.append(themplis)
 
-        if not os.path.exists('./csvs/'+name+'/'):
-            os.makedirs('./csvs/'+name+"/")
+        if not os.path.exists(dirc):
+            os.makedirs(dirc)
             print("path successfuly created")
         else:
             print("folder found ")
-
         ## noft -> number of trades
+        address=dirc+"/"+filename+fileformat 
         df=pd.DataFrame(columns=["date","open","high","low","close","volume","noft"],data=dfdata)
         df.set_index("date",drop=True,inplace=True)
-        df.to_csv("./csvs/"+name+"/"+startff+"-"+endff+"-"+name+"-"+interval+".csv",mode="w")
+        df.to_csv(address,mode="w")
         print("successfuly created")
-        return "./csvs/"+name+"/"+startff+"-"+endff+"-"+name+"-"+interval+".csv"
+        return address
 # %%this function use to update chart symbols and last price of them 
-def updateMainChart():
+# a=get_Kline_csv("ADAUSDT")
+# name="amir"
+# interval="1d"
+# a=mainPath+"/data/csvs/"+name+"/"+interval
+a=get_Kline_csv(name="ADAUSDT")
+a
+# %%this function use to update chart symbols and last price of them 
+def update_Main_Chart():
     info=client.get_all_tickers()
     name="latest-chart"
     symdf=pd.DataFrame(columns=["symbol","price"],data=info)
@@ -69,9 +85,24 @@ def updateMainChart():
     else:
         print("folder found ")
 
-    symdf.to_csv("./csvs/"+name+"/mainChart.csv",mode="w")
+    symdf.to_csv("./csvs/"+name+"/mainChart.csv",mode="w",index=True)
     return "./csvs/"+name+"/mainChart.csv"
-# %% def test area
+# %%
+def get_all_hisData(interval="1d",startin=dat(2020,1,1,00,1,1),endin=dat.now()):
+    address = update_Main_Chart() 
+    df =pd.read_csv(address)
+    addressBook=[]
+    for x in range(0,df.size-1):
+        tempdata={}
+        symbol=df["symbol"][x]
+        interval="1d"
+        symdata=get_Kline_csv(symbol,interval=interval,startin=startin,endin=dat.now())
+        tempdata={"symbol":symbol,"address":symdata,"start":startin.strftime("%Y-%m-%d %H:%M:%S"),"end":endin.strftime("%Y-%m-%d %H:%M:%S")}
+        addressBook.append(tempdata)
+    return addressBook
+# %% 
+# %% 
+# def test area
 ##get_Kline_csv("BTCUSDT",interval="1d",startin=dat(2020,9,10,00,1,1),endin=dat(2020,12,10,00,1,1))
 
 # %%data sample
